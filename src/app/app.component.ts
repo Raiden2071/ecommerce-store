5be5557a5d7204dd970be3d22e1eefa9e0951f87
slava-danilchak <danilchakv@gmail.com>
2025-03-03 20:53:46 +0200

    Add header and base styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, Injector, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './components/test/test.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title: any = 'ngrx-app';
+export class AppComponent implements OnInit {
+  title = 'ngrx-app';
+  injector = inject(Injector);
+
+  ngOnInit(): void {
+      console.log('injector', this.injector);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-  ],
+    // provideStore({ products: productReducer }),
+    // provideEffects([ProductEffects]),
+    provideHttpClient(),
+  ]
 };
