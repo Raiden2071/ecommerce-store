32c20f15069f3a957b7d2b8adcf14e8f1d77a0a5
slava-danilchak <danilchakv@gmail.com>
2025-03-04 17:05:50 +0200

    Add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
