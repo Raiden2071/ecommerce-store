9614584e678252e40afb488715888a46906088a8
slava-danilchak <danilchakv@gmail.com>
2025-03-06 17:57:54 +0200

    Add libs for ngrx and add folder architecture for book

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     // provideStore({ products: productReducer }),
     // provideEffects([ProductEffects]),
     provideHttpClient(),
-  ],
+    provideStore()
+],
 };
